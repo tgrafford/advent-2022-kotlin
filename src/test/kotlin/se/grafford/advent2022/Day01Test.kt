44415b5496364d03e0f52efe9b0f227561248025
Tim Grafford <tgrafford@gmail.com>
2022-12-01 15:29:41 +0100

    Day 01 - Rename test scenarios

--- src/test/kotlin/se/grafford/advent2022/Day01Test.kt
+++ src/test/kotlin/se/grafford/advent2022/Day01Test.kt
@@ -23,21 +23,21 @@ class Day01Test : ShouldSpec({
     """.trimIndent().lines()
 
     context("Part 1") {
-        should("match example") {
+        should("matches example") {
             Day01(example).solvePart1() shouldBe 24000
         }
 
-        should("solve the actual answer") {
+        should("solves the answer") {
             Day01(readResource("day01.txt")).solvePart1() shouldBe 69626
         }
     }
 
     context("Part 2") {
-        should("match example") {
+        should("matches example") {
             Day01(example).solvePart2() shouldBe 45000
         }
 
-        should("solve the actual answer") {
+        should("solves the answer") {
             Day01(readResource("day01.txt")).solvePart2() shouldBe 206780
         }
     }
